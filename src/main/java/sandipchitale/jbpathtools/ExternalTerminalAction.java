93f195ee9e0cff1909924b01b9fbbce2b1fdd301
Sandip Chitale <sandipchitale@gmail.com>
2024-11-03 18:10:37 -0800

    Refactor.

--- src/main/java/sandipchitale/jbpathtools/ExternalTerminalAction.java
+++ src/main/java/sandipchitale/jbpathtools/ExternalTerminalAction.java
@@ -1,6 +1,9 @@
 package sandipchitale.jbpathtools;
 
-import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
@@ -9,44 +12,22 @@ import java.io.File;
 
 public class ExternalTerminalAction extends AnAction {
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
+    public void actionPerformed(@NotNull AnActionEvent actionEvent) {
         VirtualFile virtualFile = null;
-        Editor editor = CommonDataKeys.EDITOR.getData(e.getDataContext());
-        if (editor != null) {
-            virtualFile = editor.getVirtualFile();
+        Editor editor = CommonDataKeys.EDITOR.getData(actionEvent.getDataContext());
+        if (editor == null) {
+            virtualFile = actionEvent.getDataContext().getData(CommonDataKeys.VIRTUAL_FILE);
         } else {
-            VirtualFile[] virtualFileArray = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
-            if (virtualFileArray != null && virtualFileArray.length > 0) {
-                virtualFile = virtualFileArray[0];
-            }
+            virtualFile = editor.getVirtualFile();
         }
 
         if (virtualFile != null) {
-            explore(new File(virtualFile.getPath()));
+            ExternalTerminalService.externalTerminal(new File(virtualFile.getPath()));
         }
-    }
-
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-        e.getPresentation().setEnabled(
-                PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext()) != null || CommonDataKeys.EDITOR.getData(e.getDataContext()) != null);
     }
 
     @Override
     public @NotNull ActionUpdateThread getActionUpdateThread() {
         return ActionUpdateThread.EDT;
-    }
-
-    public static void explore(File file) {
-        // Get the configured explorer commands for folder and file
-        if (file != null && file.exists()) {
-            String exploreCommand;
-            if (file.isDirectory()) {
-                exploreCommand = PathToolsPreferences.getFolderShellCommand();
-            } else {
-                exploreCommand = PathToolsPreferences.getFileShellCommand();
-            }
-            Utilities.launch(exploreCommand, file);
-        }
     }
 }
