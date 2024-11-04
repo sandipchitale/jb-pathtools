79f33c07af50980629b9c03898cbb818cfa9d925
Sandip Chitale <sandipchitale@gmail.com>
2024-11-03 19:39:22 -0800

    Formatting.

--- src/main/java/sandipchitale/jbpathtools/Utilities.java
+++ src/main/java/sandipchitale/jbpathtools/Utilities.java
@@ -8,15 +8,14 @@ import java.util.regex.Pattern;
  * This implements some utility methods.
  *
  * @author Sandip V. Chitale
- *
  */
 public class Utilities {
-	private static Pattern VARIABLE_PATTERN = Pattern.compile("\\{([a-z-]+)\\}");
+    private static Pattern VARIABLE_PATTERN = Pattern.compile("\\{([a-z-]+)\\}");
 
-	static void launch(String command, File file) {
-		// Launch the explore command
-		CommandLauncher.launch(processCommand(command, file));
-	}
+    static void launch(String command, File file) {
+        // Launch the explore command
+        CommandLauncher.launch(processCommand(command, file));
+    }
 
 
 //	private static String[] escapeBackslash(String[] paths) {
@@ -39,77 +38,77 @@ public class Utilities {
 //				.replaceAll(Pattern.quote(PathToolsPreferences.FILE_PARENT_NAME), "\\$$0");
 //	}
 
-	/**
-	 *
-	 * <ul>
-	 * <li><code>${path}</code></li>
-	 * <li><code>${path-slashes}</code></li>
-	 * <li><code>${path-backslashes}</code></li>
-	 * <li><code>${parent-path}</code></li>
-	 * <li><code>${parent-path-slashes}</code></li>
-	 * <li><code>${parent-path-backslashes}</code></li>
-	 * <li><code>${name}</code></li>
-	 * <li><code>${name-sans-extension}</code></li>
-	 * <li><code>${extension}</code></li>
-	 * <li><code>${parent-path}</code></li>
-	 * <li><code>${parent-name}</code></li>
-	 * <li><code>${parent-name-sans-extension}</code></li>
-	 * <li><code>${parent-extension}</code></li>
-	 * </ul>
-	 *
-	 * @param command
-	 * @param file
-	 * @return
-	 */
+    /**
+     * <ul>
+     * <li><code>${path}</code></li>
+     * <li><code>${path-slashes}</code></li>
+     * <li><code>${path-backslashes}</code></li>
+     * <li><code>${parent-path}</code></li>
+     * <li><code>${parent-path-slashes}</code></li>
+     * <li><code>${parent-path-backslashes}</code></li>
+     * <li><code>${name}</code></li>
+     * <li><code>${name-sans-extension}</code></li>
+     * <li><code>${extension}</code></li>
+     * <li><code>${parent-path}</code></li>
+     * <li><code>${parent-name}</code></li>
+     * <li><code>${parent-name-sans-extension}</code></li>
+     * <li><code>${parent-extension}</code></li>
+     * </ul>
+     *
+     * @param command
+     * @param file
+     * @return
+     */
 
-	static String processCommand(String command, File file) {
-		File parentFile = file.getParentFile();
-		return VARIABLE_PATTERN.matcher(command).replaceAll((MatchResult matchResult) -> {
-			String variableName = matchResult.group(0);
-			switch (variableName) {
-				case PathToolsPreferences.FILE_PATH :
-					return file.getAbsolutePath();
-				case PathToolsPreferences.FILE_PATH_SLASHES :
-					return file.getAbsolutePath().replace('\\', '/');
-				case PathToolsPreferences.FILE_PATH_BACKSLASHES :
-					return file.getAbsolutePath().replace('/', '\\');
-				case PathToolsPreferences.FILE_NAME :
-					return file.getName();
-				case PathToolsPreferences.FILE_NAME_SANS_EXTENSION :
-					return splitNameAndExtension(file.getName())[0];
-				case PathToolsPreferences.FILE_EXTENSION :
-					return splitNameAndExtension(file.getName())[1];
-				case PathToolsPreferences.FILE_PARENT_NAME :
-					return parentFile.getName();
-				case "parent-name-sans-extension" :
-					return splitNameAndExtension(parentFile.getName())[0];
-				case "parent-extension" :
-					return splitNameAndExtension(parentFile.getName())[1];
-				case PathToolsPreferences.FILE_PARENT_PATH:
-					return parentFile.getAbsolutePath();
-				case PathToolsPreferences.FILE_PARENT_PATH_SLASHES :
-					return parentFile.getAbsolutePath().replace('\\', '/');
-				case PathToolsPreferences.FILE_PARENT_PATH_BACKSLASHES :
-					return parentFile.getAbsolutePath().replace('/', '\\');
-			}
-			return "{" + variableName + "}";
-		});
-	}
+    static String processCommand(String command, File file) {
+        File parentFile = file.getParentFile();
+        return VARIABLE_PATTERN.matcher(command).replaceAll((MatchResult matchResult) -> {
+            String variableName = matchResult.group(0);
+            switch (variableName) {
+                case PathToolsPreferences.FILE_PATH:
+                    return file.getAbsolutePath();
+                case PathToolsPreferences.FILE_PATH_SLASHES:
+                    return file.getAbsolutePath().replace('\\', '/');
+                case PathToolsPreferences.FILE_PATH_BACKSLASHES:
+                    return file.getAbsolutePath().replace('/', '\\');
+                case PathToolsPreferences.FILE_NAME:
+                    return file.getName();
+                case PathToolsPreferences.FILE_NAME_SANS_EXTENSION:
+                    return splitNameAndExtension(file.getName())[0];
+                case PathToolsPreferences.FILE_EXTENSION:
+                    return splitNameAndExtension(file.getName())[1];
+                case PathToolsPreferences.FILE_PARENT_NAME:
+                    return parentFile.getName();
+                case "parent-name-sans-extension":
+                    return splitNameAndExtension(parentFile.getName())[0];
+                case "parent-extension":
+                    return splitNameAndExtension(parentFile.getName())[1];
+                case PathToolsPreferences.FILE_PARENT_PATH:
+                    return parentFile.getAbsolutePath();
+                case PathToolsPreferences.FILE_PARENT_PATH_SLASHES:
+                    return parentFile.getAbsolutePath().replace('\\', '/');
+                case PathToolsPreferences.FILE_PARENT_PATH_BACKSLASHES:
+                    return parentFile.getAbsolutePath().replace('/', '\\');
+            }
+            return "{" + variableName + "}";
+        });
+    }
 
-	/**
-	 * Return the name without extension and extension for a name.
-	 * @param nameAndExtension
-	 * @return the name without extension and extension for a name.
-	 */
-	private static String[] splitNameAndExtension(String nameAndExtension) {
-		String[] nameAndExtensionArray = new String[] { nameAndExtension, "" };
-		int lastIndexOfDot = nameAndExtension.lastIndexOf('.');
-		if (lastIndexOfDot != -1) {
-			nameAndExtensionArray[0] = nameAndExtension.substring(0,
-					lastIndexOfDot);
-			nameAndExtensionArray[1] = nameAndExtension
-					.substring(lastIndexOfDot + 1);
-		}
-		return nameAndExtensionArray;
-	}
+    /**
+     * Return the name without extension and extension for a name.
+     *
+     * @param nameAndExtension
+     * @return the name without extension and extension for a name.
+     */
+    private static String[] splitNameAndExtension(String nameAndExtension) {
+        String[] nameAndExtensionArray = new String[]{nameAndExtension, ""};
+        int lastIndexOfDot = nameAndExtension.lastIndexOf('.');
+        if (lastIndexOfDot != -1) {
+            nameAndExtensionArray[0] = nameAndExtension.substring(0,
+                    lastIndexOfDot);
+            nameAndExtensionArray[1] = nameAndExtension
+                    .substring(lastIndexOfDot + 1);
+        }
+        return nameAndExtensionArray;
+    }
 }
